bcda9998a9745f045f28c572abfb054f098daabd
Justifall <84130430+Angst04@users.noreply.github.com>
2023-09-19 22:05:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let tg = window.Telegram.WebApp;
+const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+let theme = document.getElementById('theme');
+if (prefersDarkMode) {
+  theme.href = 'dark-mode.css'
+} else {
+  theme.href = 'light-mode.css'
+};
+
+console.log(tg.InitData())
